c7e04e53061fd833a792539d9b34eb7daa4f2d70
ELDADBRHANO <ebrhano@gmail.com>
2022-05-26 18:58:40 +0300

    second_commit

--- main.js
+++ main.js
@@ -8,14 +8,38 @@
 
 
 // 3
-var collection = document.getElementsByClassName("textE");
-console.log(collection);
+// var collection = document.getElementsByClassName("textE");
+// console.log(collection);
 
 
 // 4
-console.log(collection[0]);
+// console.log(collection[1]);
 
 // 5
-console.log(collection[1]);
+// console.log(collection[2].innerText);
+
+// 6
+// console.log(document.getElementsByClassName("h1_same"))
+
+// 7
+// function getNameFromUser() {
+    //  var numUser =+prompt()
+//   document.write("<h2>"+userNum+"</h2>")
+// }
+// getNameFromUser()
 
 
+// 8
+// function getFromUserNumAndColor() {
+//   var numUser=prompt("enter num")
+//   var userColor=prompt("enter color")
+//   for(var i = 0 ; i < numUser; i++){
+//     document.write("<span>"+" "+userColor+" "+"</span>")
+//   }
+// }
+// getFromUserNumAndColor()
+
+
+
+
+// 9
