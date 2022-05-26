ed76e7783862228711f4cfb31a847abb7d728baf
ELDADBRHANO <ebrhano@gmail.com>
2022-05-26 16:28:07 +0300

    commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// 1
+// console.log(h1_id.innerText)
+
+
+
+// 2
+// document.write(h3_.textContent+"my dom app");
+
+
+// 3
+var collection = document.getElementsByClassName("textE");
+console.log(collection);
+
+
+// 4
+console.log(collection[0]);
+
+// 5
+console.log(collection[1]);
+
+
